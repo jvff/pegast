f92b24a5e3e6a90bb4d987589c07b7973c2ac8cc
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2019-12-31 17:00:15 -0300

    Allow using `Vec` as zero or more repetitions

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -4,5 +4,6 @@ pub mod literal;
 mod not;
 mod optional;
 pub mod range;
+mod zero_or_more;
 
 pub use self::{any::Any, literal::Literal, not::Not, range::Range};
---
+++ src/rules/zero_or_more.rs
@@ -0,0 +1,28 @@
+use {
+    crate::{input::Input, ParseError, PegAstNode},
+    std::{borrow::Cow, iter},
+};
+
+impl<R: PegAstNode> PegAstNode for Vec<R> {
+    fn parse(input: &mut impl Input) -> Result<Self, ParseError> {
+        Ok(iter::from_fn(|| R::parse(input).ok()).collect())
+    }
+
+    fn parsed_string(&self) -> Cow<'_, str> {
+        let mut string = String::new();
+
+        for element in self {
+            string.push_str(element.parsed_string().as_ref());
+        }
+
+        Cow::Owned(string)
+    }
+
+    fn expecting() -> Vec<String> {
+        let mut expecting = R::expecting();
+
+        expecting.push("or nothing".to_owned());
+
+        expecting
+    }
+}
