7c04bdc2ceded2ae8a8ef0cc1ea675deb2c831d6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2019-12-31 15:54:36 -0300

    Create `ParseError` type

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,35 @@
 pub mod input;
+
+use std::{
+    error::Error,
+    fmt::{self, Display, Formatter},
+};
+
+#[derive(Debug)]
+pub struct ParseError {
+    pub expected: Vec<String>,
+    pub position: usize,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        writeln!(formatter, "Failed to parse input at {}", self.position)?;
+        write!(formatter, "  Expected:")?;
+
+        match self.expected.len() {
+            0 => writeln!(formatter, " nothing")?,
+            1 => writeln!(formatter, " {}", self.expected[0])?,
+            _ => {
+                writeln!(formatter, " one of:")?;
+
+                for element in &self.expected {
+                    writeln!(formatter, "    - {}", element)?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Error for ParseError {}
