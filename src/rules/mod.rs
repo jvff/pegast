826ac1a718659b65237e4aead202d2205062aa00
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2019-12-31 17:01:43 -0300

    Create `OneOrMore` rule

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -2,8 +2,9 @@ mod all_of;
 mod any;
 pub mod literal;
 mod not;
+mod one_or_more;
 mod optional;
 pub mod range;
 mod zero_or_more;
 
-pub use self::{any::Any, literal::Literal, not::Not, range::Range};
+pub use self::{any::Any, literal::Literal, not::Not, one_or_more::OneOrMore, range::Range};
---
+++ src/rules/one_or_more.rs
@@ -0,0 +1,53 @@
+use {
+    crate::{input::Input, ParseError, PegAstNode},
+    std::{borrow::Cow, iter},
+};
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct OneOrMore<T> {
+    pub head: T,
+    pub tail: Vec<T>,
+}
+
+impl<T> OneOrMore<T> {
+    pub fn iter(&self) -> impl Iterator<Item = &T> {
+        iter::once(&self.head).chain(self.tail.iter())
+    }
+}
+
+impl<T> PegAstNode for OneOrMore<T>
+where
+    T: PegAstNode,
+{
+    fn parse(input: &mut impl Input) -> Result<Self, ParseError> {
+        let (new_position, result) = {
+            let mut peek_input = input.peek_only();
+            let head = T::parse(&mut peek_input)?;
+            let tail = Vec::parse(&mut peek_input)?;
+
+            (peek_input.position(), OneOrMore { head, tail })
+        };
+
+        input.advance_to(new_position);
+
+        Ok(result)
+    }
+
+    fn parsed_string(&self) -> Cow<'_, str> {
+        if self.tail.is_empty() {
+            self.head.parsed_string()
+        } else {
+            let mut string = self.head.parsed_string().into_owned();
+
+            for item in &self.tail {
+                string.push_str(&item.parsed_string());
+            }
+
+            Cow::Owned(string)
+        }
+    }
+
+    fn expecting() -> Vec<String> {
+        T::expecting()
+    }
+}
