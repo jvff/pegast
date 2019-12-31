1e38a063572b25610ed5666545985b33d4e4023b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2019-12-31 15:35:09 -0300

    Create `Input` trait

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,1 +1,11 @@
 mod buffered_iterator;
+
+pub trait Input {
+    fn position(&self) -> usize;
+    fn advance(&mut self, amount: usize);
+    fn advance_to(&mut self, position: usize);
+    fn check(&mut self, string: &str) -> bool;
+    fn consume(&mut self, string: &str) -> bool;
+    fn next(&mut self) -> Option<char>;
+    fn peek(&mut self) -> Option<char>;
+}
