3015a58c9175b964d34f5479b070b66392b7079e
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2019-12-31 15:29:19 -0300

    Create `BufferedIterator` helper type

---
+++ src/input/buffered_iterator.rs
@@ -0,0 +1,74 @@
+use std::collections::VecDeque;
+
+pub struct BufferedIterator<T: Iterator> {
+    buffer: VecDeque<T::Item>,
+    source: T,
+}
+
+impl<T> BufferedIterator<T>
+where
+    T: Iterator,
+{
+    pub fn new(elements: impl IntoIterator<Item = T::Item, IntoIter = T>) -> Self {
+        BufferedIterator {
+            buffer: VecDeque::new(),
+            source: elements.into_iter(),
+        }
+    }
+
+    pub fn peek(&mut self, count: usize) -> impl Iterator<Item = &T::Item> {
+        if count > self.buffer.len() {
+            for _ in 0..(count - self.buffer.len()) {
+                match self.source.next() {
+                    Some(item) => self.buffer.push_back(item),
+                    None => break,
+                }
+            }
+        }
+
+        self.buffer.iter().take(count)
+    }
+
+    pub fn advance(&mut self, count: usize) -> usize {
+        let initial_buffer_length = self.buffer.len();
+
+        for _ in 0..count {
+            if self.buffer.pop_front().is_none() {
+                break;
+            }
+        }
+
+        let mut removed_count = initial_buffer_length;
+
+        if count > removed_count {
+            for _ in 0..(count - removed_count) {
+                if self.source.next().is_none() {
+                    break;
+                }
+
+                removed_count += 1;
+            }
+        }
+
+        removed_count
+    }
+
+    pub fn peek_into<'a>(&'a mut self) -> PeekingIntoBufferedIterator<'a, T> {
+        PeekingIntoBufferedIterator::new(self)
+    }
+}
+
+impl<T> Iterator for BufferedIterator<T>
+where
+    T: Iterator<Item = char>,
+{
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.buffer.len() > 0 {
+            self.buffer.pop_front()
+        } else {
+            self.source.next()
+        }
+    }
+}
