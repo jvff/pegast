2d791c71778ead2256c3aa835d26f55f2de16f6f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2020-01-01 21:18:03 +0000

    Stub derive implementation for `PegAstNode`

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,7 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(PegAstNode)]
+pub fn derive_peg_ast(input: TokenStream) -> TokenStream {
+    todo!();
 }
