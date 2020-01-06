5bfa82b2735ee9177b8f2ee71c3e73f716dfcb13
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2020-01-06 17:05:01 +0000

    Implement `parsed_string_length` for `Option<T>`

--- src/rules/optional.rs
+++ src/rules/optional.rs
@@ -15,6 +15,12 @@ impl<R: PegAstNode> PegAstNode for Option<R> {
         }
     }
 
+    fn parsed_string_length(&self) -> usize {
+        self.as_ref()
+            .map(PegAstNode::parsed_string_length)
+            .unwrap_or(0)
+    }
+
     fn expecting() -> Vec<String> {
         let mut expecting = R::expecting();
 
